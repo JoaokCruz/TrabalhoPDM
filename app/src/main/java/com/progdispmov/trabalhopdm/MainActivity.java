56648dc321c2a588b66e457840043f11fdb66bf2
JoaokCruz <jvcruz2@hotmail.com>
2021-01-02 15:40:39 -0300

    Ajustes

--- app/src/main/java/com/progdispmov/trabalhopdm/MainActivity.java
+++ app/src/main/java/com/progdispmov/trabalhopdm/MainActivity.java
@@ -3,6 +3,10 @@ package com.progdispmov.trabalhopdm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new ConexaoBD().OpenConnection();
+
+
+        System.out.println("MySQL Connect Example.");
+        Connection conn = null;
+        String url = "jdbc:mysql://jcruz1.mysql.pythonanywhere-services.com/";
+        String dbName = "trabalho";
+        String driver = "com.mysql.jdbc.Driver";
+        String userName = "jcruz1";
+        String password = "trabalhopdm";
+        try {
+            Class.forName(driver).newInstance();
+            conn = DriverManager.getConnection(url+dbName,userName,password);
+            Toast.makeText(getApplicationContext(), "Conexao aberta!", Toast.LENGTH_SHORT).show();
+            System.out.println("Connected to the database");
+            conn.close();
+            System.out.println("Disconnected from database");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
