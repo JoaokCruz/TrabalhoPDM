ce240b1fb91d7d14f43301d3eaaabfd2b7066a57
Lucas Frantz <maxkronus@gmail.com>
2021-01-02 18:20:41 -0300

    Acao do botao de adicionar novo registro

--- app/src/main/java/com/progdispmov/trabalhopdm/HomeActivity.java
+++ app/src/main/java/com/progdispmov/trabalhopdm/HomeActivity.java
@@ -2,13 +2,28 @@ package com.progdispmov.trabalhopdm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
+    Button btnEntry;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        btnEntry = findViewById( R.id.btnNewEntry );
+
+        btnEntry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( HomeActivity.this, NewRecordActivity.class );
+
+                startActivity( intent );
+            }
+        });
     }
 }
