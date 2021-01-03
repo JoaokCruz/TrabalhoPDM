6a4809ab59aa85b4853097b7c44d7b0c9b726e16
JoaokCruz <jvcruz2@hotmail.com>
2021-01-03 16:07:37 -0300

    Tela login -> cadastro

--- app/src/main/java/com/progdispmov/trabalhopdm/LoginActivity.java
+++ app/src/main/java/com/progdispmov/trabalhopdm/LoginActivity.java
@@ -2,13 +2,27 @@ package com.progdispmov.trabalhopdm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+    }
+
+
+    public void onClickCadastro(View view) {
+        Intent intent = new Intent(view.getContext(),SigninActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickLogin(View view) {
     }
 }
