cdd9a6d7f0e6ea508aa0c2b9d5e7ea440a2cc425
Lucas Frantz <maxkronus@gmail.com>
2021-01-03 15:43:33 -0300

    ajustes conexao

--- app/src/main/java/com/progdispmov/trabalhopdm/ConexaoDB.java
+++ app/src/main/java/com/progdispmov/trabalhopdm/ConexaoDB.java
@@ -1,5 +1,7 @@
 package com.progdispmov.trabalhopdm;
 
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -16,23 +18,21 @@ import com.android.volley.toolbox.Volley;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ConexaoDB extends AppCompatActivity{
-    public ConexaoDB() {
-        Inserir();
-    }
+public class ConexaoDB{
 
-    public void Inserir(){
-       RequestQueue queue = Volley.newRequestQueue(this);
+
+    public void Inserir( final Context context ){
+       RequestQueue queue = Volley.newRequestQueue(context);
        String url ="http://teyis.pythonanywhere.com/register";
        StringRequest sr = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
            @Override
            public void onResponse(String response) {
-               Toast.makeText(getApplicationContext(), response, Toast.LENGTH_SHORT).show();
+               Toast.makeText(context, response, Toast.LENGTH_SHORT).show();
            }
        }, new Response.ErrorListener() {
            @Override
            public void onErrorResponse(VolleyError error) {
-               Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_SHORT).show();
+               Toast.makeText(context, error.toString(), Toast.LENGTH_SHORT).show();
            }
        }) {
            @Override
--- app/src/main/java/com/progdispmov/trabalhopdm/MapActivity.java
+++ app/src/main/java/com/progdispmov/trabalhopdm/MapActivity.java
@@ -42,42 +42,6 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-        // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="http://teyis.pythonanywhere.com/register";
-
-
-        StringRequest sr = new StringRequest(Request.Method.POST,url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                Toast.makeText(getApplicationContext(), response, Toast.LENGTH_SHORT).show();
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_SHORT).show();
-            }
-        }){
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                Map<String,String> params = new HashMap<String, String>();
-                params.put("name","joao");
-                params.put("email","joao@gmail.com");
-                params.put("senha", "123");
-
-                return params;
-            }
-
-//            @Override
-//            public Map<String, String> getHeaders() throws AuthFailureError {
-//                Map<String,String> params = new HashMap<String, String>();
-//                params.put("Content-Type","application/x-www-form-urlencoded");
-//                return params;
-//            }
-        };
-        queue.add(sr);
-
-
 
         mapView = (MapView) findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
