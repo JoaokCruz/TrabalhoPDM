e69092aa6dcde7e416b639a24e0723442c5b8448
Lucas Frantz <maxkronus@gmail.com>
2021-01-03 19:02:01 -0300

    map activity

--- app/src/main/java/com/progdispmov/trabalhopdm/MapActivity.java
+++ app/src/main/java/com/progdispmov/trabalhopdm/MapActivity.java
@@ -23,13 +23,19 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
@@ -41,13 +47,43 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-
+        Integer id = Integer.valueOf( getIntent().getExtras().get("id").toString() );
 
         mapView = (MapView) findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
 
         mapView.getMapAsync(this);
 
+        ConexaoDB conexaoDB = new ConexaoDB();
+
+        conexaoDB.getGeolocals( getApplicationContext(), this, id );
+
+    }
+
+    public void setMap( JSONObject obj ){
+        PolylineOptions line = new PolylineOptions();
+        List<LatLng> latLngs = new ArrayList<>();
+
+        Iterator iterator = obj.keys();
+
+        do
+        {
+            String key = String.valueOf( iterator.next() );
+            JSONObject keyObj = new JSONObject();
+            try {
+                keyObj = obj.getJSONObject( key );
+                Double latitude = Double.valueOf((Double) keyObj.get("latitude"));
+                Double longitude = Double.valueOf((Double) keyObj.get("longitude"));
+                latLngs.add( new LatLng(latitude, longitude) );
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }while( iterator.hasNext() );
+
+        line.addAll( latLngs );
+        map.addPolyline( line );
 
     }
 
@@ -60,7 +96,6 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         }
         map.setMyLocationEnabled(true);
 
-        map.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(43.1, -87.9)));
     }
 
     @Override
